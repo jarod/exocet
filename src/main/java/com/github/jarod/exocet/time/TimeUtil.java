c902f7ee6b5e36d4e654dfed9ea4328cac9467b6
Jarod Liu <liuyuanzhi@gmail.com>
2015-09-06 15:06:07 +0800

    TimeUtil: add today begin time

--- src/main/java/com/github/jarod/exocet/time/TimeUtil.java
+++ src/main/java/com/github/jarod/exocet/time/TimeUtil.java
@@ -1,12 +1,19 @@
 package com.github.jarod.exocet.time;
 
 import java.sql.Timestamp;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.WeekFields;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import com.github.jarod.exocet.concurrent.ExocetExecutor;
 
 public class TimeUtil {
+	private static final ZoneOffset ZONE_OFFSET = ZoneOffset.ofHours(8);
 	protected volatile static long _systemTime = System.currentTimeMillis();
 
 	public static final long MILLIS_PER_SECOND = 1000L;
@@ -21,6 +28,24 @@ public class TimeUtil {
 	public static final int SECONDS_PER_MONTH = SECONDS_PER_DAY * 30;
 
 	private static final long systemTimeTick = Long.parseLong(System.getProperty("systime.time.tick", "200"));
+	
+	private volatile static long _dayBeginTime;
+
+	static {
+		calDayBeginTime();
+	}
+
+	private static void calDayBeginTime() {
+		final Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Date());
+		calendar.clear(Calendar.MILLISECOND);
+		calendar.clear(Calendar.SECOND);
+		calendar.clear(Calendar.MINUTE);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		_dayBeginTime = calendar.getTimeInMillis();
+		final long delay = (_dayBeginTime + TimeUtil.MILLIS_PER_DAY) - _systemTime;
+		ExocetExecutor.schedule(() -> calDayBeginTime(), delay, TimeUnit.MILLISECONDS);
+	}
 
 	static {
 		ExocetExecutor.scheduleAtFixedRate(new Runnable() {
@@ -49,5 +74,25 @@ public class TimeUtil {
 
 	public static Timestamp timestampNow() {
 		return new Timestamp(_systemTime);
+	}
+	
+	public static long todayBeginTime() {
+		return _dayBeginTime;
+	}
+	
+	public static LocalDateTime localDateTimeNow() {
+		final long second = _systemTime / MILLIS_PER_SECOND;
+		final int nano = (int) (_systemTime % MILLIS_PER_SECOND) * 1_000_000;
+		return LocalDateTime.ofEpochSecond(second, nano, ZONE_OFFSET);
+	}
+	
+	private static final WeekFields weekFields = WeekFields.of(DayOfWeek.MONDAY, 1);
+
+	public static int weekOfYear() {
+		return weekOfYear(localDateTimeNow());
+	}
+
+	public static int weekOfYear(final TemporalAccessor accessor) {
+		return accessor.get(weekFields.weekOfYear());
 	}
 }
