e648a5e2fa8548ec1924674a98c98e76bce1d129
Jarod Liu <liuyuanzhi@gmail.com>
2016-03-29 16:25:07 +0800

    add DateTimeFormatter

--- src/main/java/com/github/jarod/exocet/time/TimeUtil.java
+++ src/main/java/com/github/jarod/exocet/time/TimeUtil.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
 import java.time.temporal.WeekFields;
 import java.util.Calendar;
@@ -27,8 +28,13 @@ public class TimeUtil {
 	public static final int SECONDS_PER_WEEK = SECONDS_PER_DAY * 7;
 	public static final int SECONDS_PER_MONTH = SECONDS_PER_DAY * 30;
 
+	/** YYYY-MM-dd HH:mm:ss */
+	public static final DateTimeFormatter FORMATTER_YMD_HMS = DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss");
+	/** YYYY-MM-dd */
+	public static final DateTimeFormatter FORMATTER_YMD = DateTimeFormatter.ofPattern("YYYY-MM-dd");
+
 	private static final long systemTimeTick = Long.parseLong(System.getProperty("systime.time.tick", "200"));
-	
+
 	private volatile static long _dayBeginTime;
 
 	static {
@@ -75,17 +81,17 @@ public class TimeUtil {
 	public static Timestamp timestampNow() {
 		return new Timestamp(_systemTime);
 	}
-	
+
 	public static long todayBeginTime() {
 		return _dayBeginTime;
 	}
-	
+
 	public static LocalDateTime localDateTimeNow() {
 		final long second = _systemTime / MILLIS_PER_SECOND;
 		final int nano = (int) (_systemTime % MILLIS_PER_SECOND) * 1_000_000;
 		return LocalDateTime.ofEpochSecond(second, nano, ZONE_OFFSET);
 	}
-	
+
 	private static final WeekFields weekFields = WeekFields.of(DayOfWeek.MONDAY, 1);
 
 	public static int weekOfYear() {
